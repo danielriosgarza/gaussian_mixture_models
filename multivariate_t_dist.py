898bc2cd7854d762a2c9654c3d7f0012b4a14158
Daniel Garza <danielriosgarza@gmail.com>
2017-03-31 10:25:30 +0200

    Update multivariate_t_dist.py

--- multivariate_t_dist.py
+++ multivariate_t_dist.py
@@ -20,3 +20,13 @@ def log_multivariate_t_dist(x_vec, d, v, mu, precision_matrix):
     p5 = 1./(2*np.linalg.slogdet(precision_matrix)[1])
     p6 = c1*(log(1+np.dot(c2.T,np.dot(precision_matrix, c2))))
     return p1-p2+p3+p4+p5-p6
+
+
+def multivariate_Gaussian_rvs(mean, covariance_matrix, n):
+    mean = np.array(mean)
+    dim = len(mean)
+    mean.shape =(dim,1)
+    L = np.linalg.cholesky(covariance_matrix)
+    z = np.random.multivariate_normal(np.zeros(dim).T, np.eye(dim), size=n)
+    xa = np.array([np.dot(L, z[i]) for i in xrange(len(z))])
+    return np.array([mean.T[0]+xa[i] for i in xrange(len(xa))])
