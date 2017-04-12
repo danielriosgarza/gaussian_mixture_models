9740e1e5299c81823740a064282940312c29c5c5
Daniel Garza <danielriosgarza@gmail.com>
2017-04-12 19:00:41 +0200

    Update Gibbs_sampler_multivariate_Gaussian_prot.py

--- Gibbs_sampler_multivariate_Gaussian_prot.py
+++ Gibbs_sampler_multivariate_Gaussian_prot.py
@@ -4,7 +4,7 @@ import numpy as np
 import scipy.stats as sts
 import useful_functions as uf
 
-def make_param_dict(k_0, v_0, mu_0, Sigma_0):
+def make_param_dict(k_0, v_0, mu_0, Sigma_0,d):
     '''make a dictionary with the prior parameters for Gibbs sampling.
     updatated parameters will be the 'up_...' entries of the dictionary,
     they begin with the same values as the priors.
@@ -26,7 +26,7 @@ def make_param_dict(k_0, v_0, mu_0, Sigma_0):
     '''
     
     return {'k_0': k_0, 'up_k_0': k_0 , 'v_0': v_0, 'up_v_0': v_0, 'mu_0':mu_0, 'up_mu_0':mu_0, \
-    'Sigma_0':Sigma_0,  'up_Sigma_0':Sigma_0}
+    'Sigma_0':Sigma_0,  'up_Sigma_0':Sigma_0, 'd':d}
 
 
 def update_param_dict(X, param_dict, chol=False):
@@ -50,32 +50,40 @@ def update_param_dict(X, param_dict, chol=False):
     k_n = param_dict['k_0']+SS['n']
     v_n = param_dict['v_0']+SS['n']
     
-    mu_prec = param_dict['mu_0']-SS['E_mu']
-    mu_prec = mu_prec.T.dot(mu_prec)
+    mu_prec = SS['E_mu']-param_dict['mu_0']
+    mu_prec.shape = (len(param_dict['mu_0']),1)
+    mu_prec = mu_prec.dot(mu_prec.T)
 
     mu_n = ((param_dict['k_0']*param_dict['mu_0'])+(k_n*SS['E_mu']))/k_n
     Sigma_n = param_dict['Sigma_0'] + SS['S_m'] + ((param_dict['k_0']*SS['n'])/k_n)*mu_prec
-    
+    Prec_n = np.linalg.inv(Sigma_n)
     if chol:
-        chky_Sigma = np.linalg.cholesky(Sigma_n)
+        chky_Sigma = np.linalg.cholesky(Prec_n)
         param_dict['chky_Sigma'] = chky_Sigma
     param_dict['up_k_0']= k_n
     param_dict['up_v_0']= v_n
     param_dict['up_mu_0']= mu_n
     param_dict['up_Sigma_0']= Sigma_n
+    param_dict['up_Prec_0'] = Prec_n
+
 
 def Gibbs_sample(param_dict, chol=False):
     
     if chol:
         Chol_prec_m = uf.Wishart_rvs(param_dict['up_v_0'], param_dict['chky_Sigma'], chol=1)
-        mu = uf.multivariate_Gaussian_rvs(param_dict['up_mu_0'], (1./param_dict['up_k_0'])*Chol_prec_m, chol=1)
+        mu = uf.multivariate_Gaussian_rvs(param_dict['up_mu_0'], (param_dict['up_k_0'])*Chol_prec_m, chol=1)
         return uf.multivariate_Gaussian_rvs(mu, Chol_prec_m, chol=1)
     else:
-        prec_m = sts.wishart(df=param_dict['up_v_0'], scale=param_dict['up_Sigma_0']).rvs()
-        S = np.linalg.inv(prec_m)
-        mu = sts.multivariate_normal(mean=param_dict['up_mu_0'], cov=(1./param_dict['up_k_0'])*S).rvs()
-        return sts.multivariate_normal(mean=param_dict['up_mu_0'], cov=S).rvs()
-    
+        
+        prec_m = uf.Wishart_rvs(df=param_dict['up_v_0'], S=param_dict['up_Prec_0'])
+        mu = uf.multivariate_Gaussian_rvs(mu=param_dict['up_mu_0'], prec_m=(param_dict['up_k_0'])*prec_m)
+        return uf.multivariate_Gaussian_rvs(mu, prec_m)
+    #    prec_m = sts.invwishart(df=param_dict['up_v_0'], scale=param_dict['up_Prec_0']).rvs()
+    #
+    #    mu = sts.multivariate_normal(mean=param_dict['up_mu_0'], cov=(1./param_dict['up_k_0'])*prec_m).rvs()
+    #    return sts.multivariate_normal(mean=mu, cov=prec_m).rvs()
+
+
 def Gibbs_sampler(param_dict, t, chol=1):
     if chol:
         s_dict = {i: Gibbs_sample(param_dict, chol=1) for i in xrange(t)}
@@ -83,15 +91,23 @@ def Gibbs_sampler(param_dict, t, chol=1):
         s_dict = {i: Gibbs_sample(param_dict, chol=0) for i in xrange(t)}        
     return s_dict
 
-d = 50
+d = 200
 n=500
-A = np.random.randint(0,100)*np.random.rand(d,d)
+A = np.random.rand(d,d)
 A = A.dot(A.T)
-mu = np.random.uniform(0,100,d)
+mu = np.random.uniform(0,1000,d)
+
+#A = np.array([[11, 0.5*(sqrt(11*2))],[0.5*(sqrt(11*2)), 2]])
+
+#mu = np.array([2, 33])
 
 X = np.random.multivariate_normal(mu, A, n)
-param_dict = make_param_dict(k_0=1, v_0=1, mu_0=np.zeros(d), Sigma_0=np.eye(d))
+param_dict = make_param_dict(k_0=1, v_0=1, mu_0=np.zeros(d), Sigma_0=np.eye(d),d=d)
 
 update_param_dict(X, param_dict, chol=1)
 
-s_dict = Gibbs_sampler(param_dict, 50, chol=0)
+np.random.seed(666)
+s_dict = Gibbs_sampler(param_dict, 500, chol=1)
+
+np.random.seed(666)
+s2_dict = Gibbs_sampler(param_dict, 500, chol=0)
