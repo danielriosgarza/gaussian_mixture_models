c7c2a592949574edca78af2ab4b969aad5f5ef26
Daniel Garza <danielriosgarza@gmail.com>
2017-04-21 02:56:25 +0200

    Update mixture_sample.py

--- mixture_sample.py
+++ mixture_sample.py
@@ -3,24 +3,12 @@ from pylab import *
 import numpy as np
 import scipy.stats as sts
 import useful_functions as uf
+import math
 
-
-
-def retrieve_sufficient_statistics_Gaussian_mix(X):
-    n = len(X)
-    d = len(X[0])
-    p1 = np.zeros((d,d))
-    for i in xrange(n):
-        b= X[i].copy()
-        b.shape=(d,1)
-        p1+= b.dot(b.T)
-    return p1
-
-
-
-def initial_assigment(n, pvals, K):
+def initial_assigment(X, n, pvals, K):
     ki_dict={i:np.zeros(n, dtype=np.bool) for i in xrange(int(K))}
     ik_dict = {}
+    im_dict={}
     indices =np.arange(n)
     np.random.shuffle(indices)
     group_assigments = np.cumsum(np.random.multinomial(n, pvals=pvals, size=1)[0])
@@ -29,64 +17,72 @@ def initial_assigment(n, pvals, K):
             ki_dict[k][indices[0:group_assigments[k]]]=1
             for i in indices[0:group_assigments[k]]:
                 ik_dict[i]=k
+                im_dict[i]=np.einsum('i,j',X[i],X[i])
         else:
             ki_dict[k][indices[group_assigments[k-1]:group_assigments[k]]]=1
             for i in indices[group_assigments[k-1]:group_assigments[k]]:
                 ik_dict[i]=k
-    return ki_dict, ik_dict
+                im_dict[i]=np.einsum('i,j',X[i],X[i])
+    return ki_dict, ik_dict, im_dict
 
-def store_chol_dict(ki_dict, X):
-    chol_dict={}
-    for i in xrange(len(ki_dict)):
-        a = retrieve_sufficient_statistics_Gaussian_mix(X[ki_dict[i]])
-        chol_dict[i] = np.linalg.cholesky(a)
-    return chol_dict
+def draw_Gaussian_parameters(ss_dict, K,pi_m):
+    for k in xrange(int(K)):
+        chol_prec_m = uf.Wishart_rvs(df = ss_dict[k]['n']-1., S = ss_dict[k]['invChol_sm'], chol=1)
+        mu = uf.multivariate_Gaussian_rvs(ss_dict[k]['em'], math.sqrt(ss_dict[k]['n'])*chol_prec_m, chol=1)
+        ss_dict[k]['ch_prec_m']=chol_prec_m
+        ss_dict[k]['mean_factor'] = ss_dict[k]['n']*np.einsum('i,j',mu,mu)#mu*mu'
+        ss_dict[k]['sum_factor'] =2*np.einsum('i,j',ss_dict[k]['sX'],mu)
+        ss_dict[k]['joint_likelihood'] = uf.multivariate_Gaussian_likelihood_cached_suff_stat(ss_dict[k])+math.log(pi_m[k])
+        ss_dict[k]['mu']=mu
+        ss_dict[k]['joint_likelihood']+= uf.multivariate_Gaussian_pdf(ss_dict[k]['mu'], ss_dict[k]['em'],ss_dict[k]['ch_prec_m'], log_form=1, chol=1)
+        ss_dict[k]['joint_likelihood']+=uf.wishart_pdf(ss_dict[k]['ch_prec_m'], ss_dict[k]['invChol_sm'],ss_dict[k]['n']-1,ss_dict[k]['d'], chol=1)
+        
+def get_initial_sufficient_statistics_dict(ki_dict, X, K, pi_m):
+    ss_dict={k:uf.store_sufficient_statistics_for_Gaussian_likelihood(X[ki_dict[k]]) for k in xrange(int(K))}
+    draw_Gaussian_parameters(ss_dict, K, pi_m)
+    return ss_dict
 
-def get_sufficient_statistics_from_chol(ss_dict, ki_dict, chol_dict, X, k):
-    ind = ki_dict[k]
-    n = sum(ki_dict[k])
-    if n<3:
-        return ss_dict[k]
-    else:
-            
-        em = np.mean(X[ind], axis=0)
-        sqn=math.sqrt(n)
-        sm = uf.cholesky_r1_update(chol_dict[k], sqn*em, down=1)
-        cpm = uf.chol_of_the_inverse(sm)
-        chol_prec_m = uf.Wishart_rvs(df = n-1, S = cpm, chol=1)
-        mu = uf.multivariate_Gaussian_rvs(em, sqn*chol_prec_m, chol=1)
-        return {'cP_m' : chol_prec_m, 'n':n, 'mu':mu}
-         
-    
-    
-    
-
-    
-def assign_n_to_k(pi, ss_dict, K, data_point):
-    p =np.array([uf.multivariate_Gaussian_pdf(data_point, ss_dict[k]['mu'], \
-ss_dict[int(k)]['cP_m'], chol=1, log_form=1) for k in xrange(int(K))])
-   
-    p = p-max(p)
-    p = exp(p)
-    p = pi*p
-    p = p/sum(p)
-    return int(np.arange(K)[np.random.multinomial(1, p)==1])         
-                
-
-def update_sample_to_chol(ki_dict, chol_dict, i, k, X, remove=False):
-    if remove:
-        ki_dict[k][i]=0
-        chol_dict[k] = uf.cholesky_r1_update(chol_dict[k], X[i], down=1)
-    else:
-        ki_dict[k][i]=1
-        chol_dict[k] = uf.cholesky_r1_update(chol_dict[k], X[i], down=1)
-
-    
-def draw_pi_0(alpha, K, ki_dict):
-    alpha_up=np.array([sum(ki_dict[k])+alpha/K for k in xrange(int(K))]) 
-    return np.random.dirichlet(alpha_up)
-
-
+def leave_one_out_likelihood(ss_dict, im_dict, ki_dict, X, K, k):
+    k_ind = np.arange(K)
+    k_ind=k_ind[k_ind!=k]
+    l={}
+    ind = np.arange(n)[ki_dict[k]]
+    for i in xrange(len(ind)):
+        cu_C_m = ss_dict[k]['C_m'].copy()
+        cu_sX = ss_dict[k]['sX'].copy()
+        cu_sf = ss_dict[k]['sum_factor'].copy()
+        ss_dict[k]['n']-=1
+        ss_dict[k]['C_m']-=im_dict[ind[i]]
+        ss_dict[k]['sX']-=2*X[ind[i]]
+        ss_dict[k]['sum_factor'] =2*np.einsum('i,j',ss_dict[k]['sX'],ss_dict[k]['mu'])
+        l1 = uf.multivariate_Gaussian_likelihood_cached_suff_stat(ss_dict[k])
+        l1+=uf.multivariate_Gaussian_pdf(ss_dict[k]['mu'],  ss_dict[k]['sX']/ ss_dict[k]['n'],ss_dict[k]['ch_prec_m'], log_form=1, chol=1)
+        l1+=uf.wishart_pdf(ss_dict[k]['ch_prec_m'], ss_dict[k]['invChol_sm'],ss_dict[k]['n']-1,ss_dict[k]['d'], chol=1)
+        l[ind[i]]=[ss_dict[k]['joint_likelihood']-l1]
+        ss_dict[k]['C_m'] = cu_C_m.copy()
+        ss_dict[k]['sX'] = cu_sX.copy()
+        ss_dict[k]['sum_factor']  = cu_sf.copy()
+        ss_dict[k]['n']+=1
+        Lo = [add_one_likelihood(ss_dict, im_dict, X, ki, ind[i]) for ki in k_ind]
+        l[ind[i]].extend(Lo) 
+    return l
+def add_one_likelihood(ss_dict, im_dict, X, k, i):
+    cu_C_m = ss_dict[k]['C_m'].copy()
+    cu_sX = ss_dict[k]['sX'].copy()
+    cu_sf = ss_dict[k]['sum_factor'].copy()
+    ss_dict[k]['n']+=1
+    ss_dict[k]['C_m']+=im_dict[i]
+    ss_dict[k]['sX']+=2*X[i]
+    ss_dict[k]['sum_factor'] =2*np.einsum('i,j',ss_dict[k]['sX'],ss_dict[k]['mu'])
+    l1 = uf.multivariate_Gaussian_likelihood_cached_suff_stat(ss_dict[k])
+    l1+=uf.multivariate_Gaussian_pdf(ss_dict[k]['mu'],ss_dict[k]['sX']/ ss_dict[k]['n'],ss_dict[k]['ch_prec_m'], log_form=1, chol=1)
+    l1+=uf.wishart_pdf(ss_dict[k]['ch_prec_m'], ss_dict[k]['invChol_sm'],ss_dict[k]['n']-1,ss_dict[k]['d'], chol=1)
+    L = l1 - ss_dict[k]['joint_likelihood']
+    ss_dict[k]['C_m'] = cu_C_m.copy()
+    ss_dict[k]['sX'] = cu_sX.copy()
+    ss_dict[k]['sum_factor']  = cu_sf.copy()
+    ss_dict[k]['n']-=1  
+    return L
 d = 2 #indexed by j
 n=600 #indexed by i
 K=3. #indexed by k
@@ -117,37 +113,7 @@ for i in xrange(n):
 
 X = np.array(X)
     
+pi_m = np.random.dirichlet(pi_0)
+ki_dict, ik_dict, im_dict = initial_assigment(X,n, p, K)
+ss_dict = get_initial_sufficient_statistics_dict(ki_dict, X, K, pi_m)
 
-ki_dict, ik_dict = initial_assigment(n, p, K)
-chol_dict = store_chol_dict(ki_dict, X)
-
-
-pi_0 = np.random.dirichlet(pi_0)
-s={}
-ss_dict = {i:get_sufficient_statistics_from_chol(s, ki_dict, chol_dict, X, i) for i in xrange(int(K))}
-
-
-
-def update_k_assignment(ik_dict,  ki_dict, chol_dict, ss_dict, pi_0, K, X):
-    for i in xrange(len(ik_dict)):
-       
-        current_assign = ik_dict[i]
-        new_assign = assign_n_to_k(pi_0, ss_dict, K,X[i])
-        if current_assign==new_assign:
-            pass
-        else:
-            update_sample_to_chol(ki_dict, chol_dict, i, current_assign, X, remove=True)
-            ss_dict[current_assign]=get_sufficient_statistics_from_chol(ss_dict, ki_dict, chol_dict, X, current_assign)
-            ik_dict[i] = new_assign
-            update_sample_to_chol(ki_dict, chol_dict, i, new_assign, X, remove=False)
-            ss_dict[new_assign]=get_sufficient_statistics_from_chol(ss_dict, ki_dict, chol_dict, X, new_assign)
-
-
-def Gibbs_sampler(t, ik_dict,  ki_dict, chol_dict, ss_dict, pi_0, alpha_0, K, X):
-    pis = pi_0.copy()
-    for i in xrange(t):
-        update_k_assignment(ik_dict,  ki_dict, chol_dict, ss_dict, pis, K, X)
-        pis= draw_pi_0(alpha_0, K, ki_dict)
-    return pis
-
-pg = Gibbs_sampler(40, ik_dict,  ki_dict, chol_dict, ss_dict, pi_0, alpha_0, K, X)
