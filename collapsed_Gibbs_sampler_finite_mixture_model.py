f0b4a0248b69fe11c314c68166f90914b1908edb
Daniel Garza <danielriosgarza@gmail.com>
2017-04-27 00:58:11 +0200

    Update collapsed_Gibbs_sampler_finite_mixture_model.py

--- collapsed_Gibbs_sampler_finite_mixture_model.py
+++ collapsed_Gibbs_sampler_finite_mixture_model.py
@@ -26,7 +26,7 @@ def initial_assigment(X, pvals, K):
 
 
 def cache_sufficient_statistics_for_n(X, K, alpha_0, kappa_0, v_0, mu_0):
-    N = X.shape[0]
+    N = X.shape[0]+1000
     D = X.shape[1]
     
     #global constants
@@ -43,13 +43,13 @@ def cache_sufficient_statistics_for_n(X, K, alpha_0, kappa_0, v_0, mu_0):
     
     l = []
     for n in xrange(N):
-        m = sum([lggm((v_0+n+2-i)/2.) for i in xrange(n)])
+        m = sum([lggm((v_0+n+2-i)/2.) for i in xrange(D)])
         l.append(m)
     l_gamma_s_2 = np.array(l)
     
     l = []
     for n in xrange(N):
-        m = -sum([lggm((v_0+n+1-i)/2.) for i in xrange(n)])
+        m = -sum([lggm((v_0+n+1-i)/2.) for i in xrange(D)])
         l.append(m)
     l_gamma_s_1 = np.array(l)
 
@@ -57,13 +57,11 @@ def cache_sufficient_statistics_for_n(X, K, alpha_0, kappa_0, v_0, mu_0):
 
     v_n = np.array([0.5*(v_0+n) for n in xrange(N)])
 
-    mu_factor = np.array([((kappa_0+mu_0)/(kappa_0+n), (1./(kappa_0+n))) for n in xrange(N)])
-
     mu_0_factor = kappa_0*np.einsum('i,j->ij',mu_0, mu_0)
 
     return {'k_coeff':k_coeff , 'alpha_coeff': alpha_coeff, 'Zconst': Zconst, 'l_kappa_p_1':l_kappa_p_1, \
     'l_kappa': l_kappa, 'l_gamma_s_2':l_gamma_s_2 , 'l_gamma_s_1':l_gamma_s_1 ,\
-    'v_n_p_1': v_n_p_1 , 'v_n': v_n, 'mu_factor': mu_factor, 'kappa_0':kappa_0, 'mu_0_factor':mu_0_factor, 'mu_0':mu_0}
+    'v_n_p_1': v_n_p_1 , 'v_n': v_n,  'kappa_0':kappa_0, 'mu_0_factor':mu_0_factor, 'mu_0':mu_0}
 
 
 
@@ -77,8 +75,7 @@ def cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, S_0):
         sX = np.einsum('nd->d',samp)
         mu_n = (n_SS['kappa_0']*n_SS['mu_0']+sX)/(n_SS['kappa_0']+n)
         S = S_0+np.einsum('ij, iz->jz', samp, samp)+n_SS['mu_0_factor']-(n_SS['kappa_0']+n)*np.einsum('i,j->ij',mu_n, mu_n)
-        
-        k_SS[k]['sX']=sX
+              
         k_SS[k]['mu_n']=mu_n
         k_SS[k]['chol_S'] = linalg.cholesky(S, lower=1, overwrite_a=1)
         k_SS[k]['n']=n
@@ -87,24 +84,25 @@ def cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, S_0):
 
 def downdate_chol_S(X_n, k,  k_SS, n_SS):
     n = k_SS[k]['n']
-    k_0 = n_SS['kappa_0']
-    mu_0 = k_SS[k]['mu_n']
-    
-    mu_n = (((k_0+n)*mu_0)-X_n)/(k_0+n+1)
-    
-    u2 = math.sqrt(k_0+n)*mu_0
-    
-    u3 = math.sqrt(k_0+n+1)*mu_n
-    
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], X_n, down=True, pychud_im=0)
-    
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u2, down=True, pychud_im=0)
-    
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u3, down=False, pychud_im=0)
-    
-    k_SS[k]['n']+=1
-    
-    k_SS[k]['mu_n']=mu_n
+    if n>5:
+        k_0 = n_SS['kappa_0']
+        mu_0 = k_SS[k]['mu_n'].copy()
+        
+        mu_n = (((k_0+n)*mu_0)-X_n)/(k_0+n-1)
+        
+        u2 = math.sqrt(k_0+n)*mu_0
+        
+        u3 = math.sqrt(k_0+n-1)*mu_n
+        
+        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], X_n, down=True)
+        
+        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u2, down=False)
+        
+        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u3, down=True)
+        
+        k_SS[k]['n']-=1
+        
+        k_SS[k]['mu_n']=mu_n
 
 
 
@@ -120,21 +118,31 @@ def update_chol_S(X_n, k,  k_SS, n_SS):
     
     u3 = math.sqrt(k_0+n+1)*mu_n
     
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], X_n, down=False, pychud_im=0)
+    chol_S = k_SS[k]['chol_S'].copy()
     
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u2, down=False, pychud_im=0)
+    chol_S = uf.cholesky_r1_update(chol_S, X_n, down=False)
     
-    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u3, down=True, pychud_im=0)
+    chol_S = uf.cholesky_r1_update(chol_S, u2, down=False)
     
-    k_SS[k]['n']+=1
+    chol_S = uf.cholesky_r1_update(chol_S, u3, down=True)
     
-    k_SS[k]['mu_n']=mu_n
+    return chol_S,mu_n
+    
+
+def normalize_L(L):
+    m = L.copy()
+    m = m-max(m)
+    m= exp(m)
+    return m/sum(m)
+    
+
+
 
 
 D = 2 #indexed by d
 N=1000 #indexed by n
 K=3 #indexed by k
-alpha_0 = 100.
+alpha_0 = 10.
 
 pi_0 = np.array([alpha_0/K for i in xrange(int(K))])
 
@@ -170,11 +178,32 @@ n_SS = cache_sufficient_statistics_for_n(X, K, alpha_0, 1, 1, np.array([0,0]))
 
 k_SS = cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, np.eye(D))
 
+p = np.zeros(K)
+chols=[]
+muns=[]
 
-X_n = X[98].copy()
-c1 = k_SS[0]['chol_S'].copy()
-downdate_chol_S(X_n, 0, k_SS, n_SS)
-c2 = k_SS[0]['chol_S'].copy()
-nk_array[98]=2
-k_SS = cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, np.eye(D))
-c3 = k_SS[0]['chol_S'].copy()
+for t in xrange(50):
+    for n in xrange(N):
+        downdate_chol_S(X[n], nk_array[n],  k_SS, n_SS)
+        
+        for k in xrange(K):
+            cup, mu_n = update_chol_S(X[n], k,  k_SS, n_SS)
+            up_det = uf.chol_log_determinant(cup)
+            chols.append(cup)
+            muns.append(mu_n)
+            low_det = uf.chol_log_determinant(k_SS[k]['chol_S'])
+            nid =  k_SS[k]['n']
+            p_z_i =math.log(n*n_SS['k_coeff']+n_SS['alpha_coeff'])
+            p_x_i = n_SS['Zconst']+n_SS['l_kappa_p_1'][nid]+n_SS[ 'v_n_p_1'][nid]*up_det + n_SS[ 'l_gamma_s_2'][nid] + n_SS['l_kappa'][nid]  +n_SS['v_n'][nid] *low_det +n_SS['l_gamma_s_1'][nid]
+            p[k] = p_z_i+p_x_i
+        new_k = np.random.choice(np.arange(K), p=normalize_L(p))
+        k_SS[new_k]['chol_S'] = chols[new_k].copy()
+        k_SS[new_k]['mu_n']=muns[new_k].copy()
+        chols=[]
+        muns=[]
+        k_SS[new_k]['n']+=1
+        nk_array[n]=new_k
+        b=[ len(X[nk_array==0]),  len(X[nk_array==1]), len(X[nk_array==2])]
+        if 1000 in b:
+            break
+        b2=b[:]
