73cab29dd6af075229569253958b9e2c8e7c64e8
Daniel Garza <danielriosgarza@gmail.com>
2017-05-01 18:34:22 +0200

    Update

--- collapsed_Gibbs_sampler_finite_mixture_model.py
+++ collapsed_Gibbs_sampler_finite_mixture_model.py
@@ -9,6 +9,23 @@ import scipy.linalg as linalg
 
 
 def initial_assigment(X, pvals, K):
+    '''create a random initial assignment of samples in X to K clusters,
+    based on some probability.
+    
+    Parameters
+    --------
+    X: array-like
+    nxd multidimensional data. n represents the number of samples (len (X)) and d the dimensionality (len(X[0])).
+    pvals: array-like
+      vector of probabilities initially assigned to each cluster K (consists of K numbers between 0 and 1 that 
+      add to 1). 
+    K: int
+      number of clusters.
+
+    Outputs
+    --------
+    nk_array: array-like
+    mapping of samples in position i to in in range(K).'''
     
     N = len(X)
     nk_array = np.zeros(N, dtype=int)
@@ -33,29 +50,29 @@ def cache_sufficient_statistics_for_n(X, K, alpha_0, kappa_0, v_0, mu_0):
     k_coeff = K/(K*(N+alpha_0-1))
     alpha_coeff = alpha_0/(K*(N+alpha_0-1))
     
-    Zconst = -0.5*D*math.log(pi)
-    
+    Zconst = np.array([(-0.5)*D*math.log(pi) for i in xrange(1,N)])
+        
     
     #indexed by n
-    l_kappa_p_1 = np.array([-0.5*D*math.log(kappa_0+n+1) for n in xrange(N)])
+    l_kappa_p_1 = np.array([-0.5*D*math.log(kappa_0+n+1) for n in xrange(1,N)])
     
-    l_kappa = np.array([0.5*D*math.log(kappa_0+n) for n in xrange(N)])
+    l_kappa = np.array([0.5*D*math.log(kappa_0+n) for n in xrange(1,N)])
     
     l = []
-    for n in xrange(N):
-        m = sum([lggm((v_0+n+2-i)/2.) for i in xrange(D)])
+    for n in xrange(1,N):
+        m = sum([lggm((v_0+n+2-i)/2.) for i in xrange(1,D+1)])
         l.append(m)
     l_gamma_s_2 = np.array(l)
     
     l = []
-    for n in xrange(N):
-        m = -sum([lggm((v_0+n+1-i)/2.) for i in xrange(D)])
+    for n in xrange(1,N):
+        m = -sum([lggm((v_0+n+1-i)/2.) for i in xrange(1,D+1)])
         l.append(m)
     l_gamma_s_1 = np.array(l)
 
-    v_n_p_1 = np.array([-0.5*(v_0+n+1) for n in xrange(N)])
+    v_n_p_1 = np.array([-0.5*(v_0+n+1) for n in xrange(1,N)])
 
-    v_n = np.array([0.5*(v_0+n) for n in xrange(N)])
+    v_n = np.array([0.5*(v_0+n) for n in xrange(1,N)])
 
     mu_0_factor = kappa_0*np.einsum('i,j->ij',mu_0, mu_0)
 
@@ -84,25 +101,24 @@ def cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, S_0):
 
 def downdate_chol_S(X_n, k,  k_SS, n_SS):
     n = k_SS[k]['n']
-    if n>5:
-        k_0 = n_SS['kappa_0']
-        mu_0 = k_SS[k]['mu_n'].copy()
+    k_0 = n_SS['kappa_0']
+    mu_0 = k_SS[k]['mu_n'].copy()
         
-        mu_n = (((k_0+n)*mu_0)-X_n)/(k_0+n-1)
+    mu_n = (((k_0+n)*mu_0)-X_n)/(k_0+n-1)
         
-        u2 = math.sqrt(k_0+n)*mu_0
+    u2 = math.sqrt(k_0+n)*mu_0
         
-        u3 = math.sqrt(k_0+n-1)*mu_n
+    u3 = math.sqrt(k_0+n-1)*mu_n
         
-        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], X_n, down=True)
+    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], X_n, down=True)
         
-        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u2, down=False)
+    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u2, down=False)
         
-        k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u3, down=True)
+    k_SS[k]['chol_S'] = uf.cholesky_r1_update(k_SS[k]['chol_S'], u3, down=True)
         
-        k_SS[k]['n']-=1
+    k_SS[k]['n']-=1
         
-        k_SS[k]['mu_n']=mu_n
+    k_SS[k]['mu_n']=mu_n
 
 
 
@@ -139,71 +155,65 @@ def normalize_L(L):
 
 
 
-D = 2 #indexed by d
-N=1000 #indexed by n
-K=3 #indexed by k
-alpha_0 = 10.
+#D = 2 #indexed by d
+#N=100 #indexed by n
+#K=3 #indexed by k
+#alpha_0 = 10.
+#
+#pi_0 = np.array([alpha_0/K for i in xrange(int(K))])
+#
+#
+#true_pi= np.array([30., 20.,50.])
+#p = true_pi/sum(true_pi)
+#
+#A1 = np.array([[11, 0.5*(sqrt(11*2))],[0.5*(sqrt(11*2)), 2]])
+#A2 = np.array([[5, 0.9*(sqrt(5*12))],[0.9*(sqrt(5*12)), 12]])
+#A3 = np.array([[8, 0.1*(sqrt(8*7))],[0.1*(sqrt(8*7)), 7]])
+#
+#
+#mu1 = np.random.uniform(-100,1000, D)
+#mu2 = np.random.uniform(-100,1000, D)
+#mu3 = np.random.uniform(-100,1000, D)
+#
+#cvms = np.array([A1, A2, A3])
+#mus = np.array([mu1, mu2, mu3])
+#
+#
+#X = []
+#
+#for i in xrange(N):
+#    ind = np.random.choice(np.arange(K), p=p)
+#    X.append(np.random.multivariate_normal(mean=mus[ind], cov=cvms[ind]))
+#
+#X = np.array(X)
 
+D = 2           # dimensions
+N = 10          # number of points to generate
+K_true = 4      # the true number of components
+
+# Model parameters
+alpha_0 = 1.
+K = 6           # number of components
+n_iter = 10
 pi_0 = np.array([alpha_0/K for i in xrange(int(K))])
+# Generate data
+mu_scale = 4.0
+covar_scale = 0.7
+z_true = np.random.randint(0, K_true, N)
+mu = np.random.randn(D, K_true)*mu_scale
+X = mu[:, z_true] + np.random.randn(D, N)*covar_scale
+X = X.T
 
 
-true_pi= np.array([300., 200.,100.])
-p = true_pi/sum(true_pi)
+#pvals = np.random.dirichlet(pi_0)
 
-A1 = np.array([[11, 0.5*(sqrt(11*2))],[0.5*(sqrt(11*2)), 2]])
-A2 = np.array([[5, 0.9*(sqrt(5*12))],[0.9*(sqrt(5*12)), 12]])
-A3 = np.array([[8, 0.1*(sqrt(8*7))],[0.1*(sqrt(8*7)), 7]])
-
-
-mu1 = np.random.uniform(-100,1000, D)
-mu2 = np.random.uniform(-100,1000, D)
-mu3 = np.random.uniform(-100,1000, D)
-
-cvms = np.array([A1, A2, A3])
-mus = np.array([mu1, mu2, mu3])
-
-
-X = []
-
-for i in xrange(N):
-    ind = np.random.choice(np.arange(K), p=p)
-    X.append(np.random.multivariate_normal(mean=mus[ind], cov=cvms[ind]))
-
-X = np.array(X)
-pvals = np.random.dirichlet(pi_0)
-
-nk_array = initial_assigment(X, pvals, K)
+nk_array = initial_assigment(X, pi_0, K)
 
 n_SS = cache_sufficient_statistics_for_n(X, K, alpha_0, 1, 1, np.array([0,0]))
 
 k_SS = cache_sufficient_statistics_for_k(nk_array, K, n_SS, X, np.eye(D))
 
-p = np.zeros(K)
+
 chols=[]
 muns=[]
 
-for t in xrange(50):
-    for n in xrange(N):
-        downdate_chol_S(X[n], nk_array[n],  k_SS, n_SS)
-        
-        for k in xrange(K):
-            cup, mu_n = update_chol_S(X[n], k,  k_SS, n_SS)
-            up_det = uf.chol_log_determinant(cup)
-            chols.append(cup)
-            muns.append(mu_n)
-            low_det = uf.chol_log_determinant(k_SS[k]['chol_S'])
-            nid =  k_SS[k]['n']
-            p_z_i =math.log(n*n_SS['k_coeff']+n_SS['alpha_coeff'])
-            p_x_i = n_SS['Zconst']+n_SS['l_kappa_p_1'][nid]+n_SS[ 'v_n_p_1'][nid]*up_det + n_SS[ 'l_gamma_s_2'][nid] + n_SS['l_kappa'][nid]  +n_SS['v_n'][nid] *low_det +n_SS['l_gamma_s_1'][nid]
-            p[k] = p_z_i+p_x_i
-        new_k = np.random.choice(np.arange(K), p=normalize_L(p))
-        k_SS[new_k]['chol_S'] = chols[new_k].copy()
-        k_SS[new_k]['mu_n']=muns[new_k].copy()
-        chols=[]
-        muns=[]
-        k_SS[new_k]['n']+=1
-        nk_array[n]=new_k
-        b=[ len(X[nk_array==0]),  len(X[nk_array==1]), len(X[nk_array==2])]
-        if 1000 in b:
-            break
-        b2=b[:]
