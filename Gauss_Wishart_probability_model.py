2121c0ddd34039ec44969d707a9b2ac7afa3f326
Daniel Garza <danielriosgarza@gmail.com>
2017-05-08 01:49:29 +0200

    Update Gauss_Wishart_probability_model.py

--- Gauss_Wishart_probability_model.py
+++ Gauss_Wishart_probability_model.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 import scipy.linalg as linalg
 from Cholesky import Cholesky
-from Gaussian_variable import Gaussian_variable
 import random
 trm = linalg.get_blas_funcs('trmm')
 from scipy.special import gammaln as gamlog
@@ -19,6 +18,8 @@ class Gauss_Wishart_probability_model:
         self.prior_lp_prec = None
         self.prior_lp_mu = None
         self.prior_lp = None
+        self.data_lp = None
+        self.joint_lp = None
             
     def __prec_mu_norm_Z(self,S, kappa,v, d):
         if v<d:
@@ -31,8 +32,8 @@ class Gauss_Wishart_probability_model:
         else:
             kt = 0.5*d*math.log(kappa)
         
-        self.prec_mu_norm_Z = (0.5*df*d*math.log(2))+ ((0.25*((d**2)+d))*math.log(math.pi)) - kt \
-        -(((df-1)/2)*Cholesky(S).log_determinant(chol_A=S))+sum(gamlog(df-np.arange(d)))
+        self.prec_mu_norm_Z = (0.5*df*d*math.log(2))+ (0.25*(d*(d+1))*math.log(math.pi)) - kt \
+        -((0.5*(df-1))*Cholesky(S).log_determinant())+sum(gamlog(0.5*(df-np.arange(d))))
         
         return self.prec_mu_norm_Z
     
@@ -47,8 +48,8 @@ class Gauss_Wishart_probability_model:
         else:
             df= v+1
         
-        self.prec_norm_Z = (0.5*(df-1)*d*math.log(2))-((0.5*(df-1))*Cholesky(S).log_determinant(chol_A=S)) + ((0.25*d*(d-1))*math.log(math.pi))\
-        +sum(gamlog(df-np.arange(d)))
+        self.prec_norm_Z = (0.5*(df-1)*d*math.log(2))-((0.5*(df-1))*Cholesky(S).log_determinant()) + ((0.25*d*(d-1))*math.log(math.pi))\
+        +sum(gamlog(0.5*(df-np.arange(d))))
          
         return self.prec_norm_Z
 
@@ -86,7 +87,34 @@ class Gauss_Wishart_probability_model:
         else:
             df= v+1
         
-        self.prior_lp = -self.__prec_mu_norm_Z(S, kappa, v,d)+((0.5*df-1-d)*Cholesky(prec).log_determinant())\
+        self.prior_lp = -self.__prec_mu_norm_Z(S, kappa, v,d)+((0.5*(df-1-d))*Cholesky(prec).log_determinant())\
         -(0.5*np.einsum('ij, ij', prec, (kappa*np.einsum('i,j->ij', mu-mu_0, mu-mu_0))+S))
         
         return self.prior_lp
+    
+    def data_lp_(self, prec, XX_T, sX, mu, n, d):
+        self.data_lp= (-0.5*(n*d)*math.log(2*math.pi)) + ((0.5*n)* Cholesky(prec).log_determinant()) - (0.5*np.einsum('ij, ij', prec, (n*np.einsum('i,j->ij', mu, mu)-2*np.einsum('i,j->ij', sX, mu) + XX_T)))
+        return self.data_lp
+    
+    def joint_lp_(self, prec, XX_T, S, mu, mu_0, sX, kappa, v, n,d):
+        if v<d:
+            df = d+1
+        else:
+            df= v+1
+        self.joint_lp =  (-0.5*(n*d)*math.log(2*math.pi)) -self.__prec_mu_norm_Z(S, kappa, v,d) + ((0.5*(df-1+n-d))*Cholesky(prec).log_determinant())\
+        -(0.5*np.einsum('ij, ij', prec, ((kappa+n)*np.einsum('i,j->ij', mu, mu) - 2*np.einsum('i,j->ij', mu, (np.dot(prec, mu_0)+sX)) +  kappa*np.einsum('i,j->ij', mu_0, mu_0)+XX_T+S)))
+        return self.joint_lp 
+    
+    def marginal_lp1_(self, cov, S, kappa, v, n,d):
+        self.marginal_lp1 = (-0.5*(n*d)*math.log(2*math.pi))+ self.__prec_mu_norm_Z(cov, n+kappa, n+v,d) - self.__prec_mu_norm_Z(S, kappa, v,d)
+        return self.marginal_lp1
+    
+    def marginal_lp2_(self, X, prec, mu, kappa, v, n, d):
+        df= v+n+1-d
+        
+        M = (((kappa+n)*df)/(kappa+n+1)) * prec
+        
+        summand = [math.log(1+(1/df)*np.einsum('i,ij,j->', Xi-mu, M, Xi-mu)) for Xi in X]
+        self.marginal_lp2 = n*gamlog(0.5*(df+d))-n*gamlog(0.5*df)+0.5*n*Cholesky(M).log_determinant()-0.5*n*d*math.log(df*math.pi) - 0.5*(df+d)*sum(summand)
+        return self.marginal_lp2
+      
